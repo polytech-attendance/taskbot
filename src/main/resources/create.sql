3b90dbd630bcbc9c4557052d1fd1c68082637be4
mariohuq <15021607+mariohuq@users.noreply.github.com>
2024-05-19 20:51:47 +0300

    add create

---
+++ src/main/resources/create.sql
@@ -0,0 +1,28 @@
+create database tasks;
+drop schema if exists task_schema cascade;
+create schema task_schema;
+
+create table task_schema.owner
+( owner_id integer generated always as identity primary key
+, telegram_id integer
+)
+
+create table task_schema.task
+( task_id integer generated always as identity primary key
+, owner_id integer references owner (owner_id) on delete restrict on update restrict
+, summary text
+, deadline timestamp with time zone
+, status integer
+, estimated_time interval
+, spent_time interval
+)
+
+create table task_schema.recurring_task
+( recurring_task_id integer generated always as identity primary key
+, owner_id integer references owner (owner_id) on delete restrict on update restrict
+, summary text
+, start timestamp with time zone
+, period interval
+, finish timestamp with time zone
+, status integer
+)
