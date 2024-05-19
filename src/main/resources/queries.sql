1ce00dc62f54424f24b829e86cd9eb138134114d
Alexandr <a.timoff3@gmail.com>
2024-05-19 21:26:18 +0300

    Add sql queiries for recurring tasks

---
+++ src/main/resources/queries.sql
@@ -0,0 +1,21 @@
+# Create
+INSERT INTO task_schema.recurring_task (owner_id, summary, start, period, finish, status)
+VALUES (1, 'Повторяющаяся задача', '2024-05-20 12:00:00', '1 day', '2024-06-20 12:00:00', 0);
+
+# getInProgress
+SELECT * FROM task_schema.recurring_task WHERE owner_id = id, status = 0
+
+# reschedule
+UPDATE task_schema.recurring_task
+SET start = 'yyyy-mm-dd hh:mm:ss', finish = 'yyyy-mm-dd hh:mm:ss', period = '1 day'
+ WHERE task_id = task_id;
+
+# set_summary
+UPDATE task_schema.recurring_task
+SET summary = 'summary'
+WHERE task_id = task_id;
+
+# mark_in_progress (for done 1)
+UPDATE task_schema.recurring_task
+SET status = 0
+WHERE task_id = task_id;
