0dac4c28e9a5dabc1ed8626e5bfaa9e8fdeb105f
Alexandr <a.timoff3@gmail.com>
2024-05-19 21:36:42 +0300

    Add sql owner_id param

--- src/main/resources/queries.sql
+++ src/main/resources/queries.sql
@@ -1,21 +1,26 @@
-# Create
+-- Create
 INSERT INTO task_schema.recurring_task (owner_id, summary, start, period, finish, status)
 VALUES (1, 'Повторяющаяся задача', '2024-05-20 12:00:00', '1 day', '2024-06-20 12:00:00', 0);
 
-# getInProgress
+-- getInProgress
 SELECT * FROM task_schema.recurring_task WHERE owner_id = id, status = 0
 
-# reschedule
+-- reschedule
 UPDATE task_schema.recurring_task
 SET start = 'yyyy-mm-dd hh:mm:ss', finish = 'yyyy-mm-dd hh:mm:ss', period = '1 day'
- WHERE task_id = task_id;
+WHERE task_id = 1 AND owner_id = 1;
 
-# set_summary
+-- set_summary
 UPDATE task_schema.recurring_task
 SET summary = 'summary'
-WHERE task_id = task_id;
+WHERE task_id = 1 AND owner_id = 1;
 
-# mark_in_progress (for done 1)
+-- mark_in_progress (for done 1)
 UPDATE task_schema.recurring_task
 SET status = 0
-WHERE task_id = task_id;
+WHERE task_id = 1 AND owner_id = 1;
+
+
+-- FOR TASK SERVICE
+
+
