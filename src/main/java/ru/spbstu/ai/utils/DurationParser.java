a21cafc1f1ae83e942aa4d08007646fcb606cf51
timofeevAS <a.timoff3@gmail.com>
2024-05-21 20:27:33 +0300

    on Utils: add human readable parser for Duration

--- src/main/java/ru/spbstu/ai/utils/DurationParser.java
+++ src/main/java/ru/spbstu/ai/utils/DurationParser.java
@@ -1,10 +1,35 @@
 package ru.spbstu.ai.utils;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DurationParser {
+
+    public static String toHumanReadableString(Duration duration) {
+        long days = duration.toDays();
+        duration = duration.minusDays(days);
+
+        long hours = duration.toHours();
+        duration = duration.minusHours(hours);
+
+        long minutes = duration.toMinutes();
+
+        List<String> parts = new ArrayList<>();
+        if (days > 0) {
+            parts.add(days + " day" + (days > 1 ? "s" : ""));
+        }
+        if (hours > 0) {
+            parts.add(hours + " hour" + (hours > 1 ? "s" : ""));
+        }
+        if (minutes > 0) {
+            parts.add(minutes + " minute" + (minutes > 1 ? "s" : ""));
+        }
+
+        return String.join(" ", parts);
+    }
 
     public static Duration parse(String input) {
         Pattern pattern = Pattern.compile("(\\d+)\\s*(day|hour|minute)(s?)", Pattern.CASE_INSENSITIVE);
