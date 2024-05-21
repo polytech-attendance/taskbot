a9d8f9c68e61a9f32c45bb4c99c2637b47f8f75c
timofeevAS <a.timoff3@gmail.com>
2024-05-21 15:56:23 +0300

    On /start: fix multi-created bug, when user write /start multiply.

--- src/main/java/ru/spbstu/ai/component/StartCommand.java
+++ src/main/java/ru/spbstu/ai/component/StartCommand.java
@@ -8,6 +8,7 @@ import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
+import reactor.core.publisher.Mono;
 import ru.spbstu.ai.service.UserService;
 
 @Component
@@ -22,7 +23,8 @@ public class StartCommand extends BotCommand {
 
     @Override
     public void execute(TelegramClient telegramClient, User user, Chat chat, String[] strings) {
-        var userDb = users.create(user.getId()).block();
+        signUp(user); // No message to reply similar for both cases: user exists, and not.
+
         StringBuilder builder = new StringBuilder();
         builder.append("Welcome ").append(user.getUserName()).append("\n")
             .append("this bot will manage your tasks!");
@@ -31,5 +33,20 @@ public class StartCommand extends BotCommand {
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void signUp(User user) {
+        users.getUser(user.getId())
+                .hasElement()  // Check if user exists
+                .flatMap(exists -> {
+                    if (exists) {
+                        return Mono.empty();
+                    } else {
+                        return users.create(user.getId())
+                                .doOnNext(newUser -> {
+                                    // TODO: Create log info output, about creating user
+                                });
+                    }
+                });
     }
 }
